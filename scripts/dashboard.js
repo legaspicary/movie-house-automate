ab58ee3bd448013a3e1b2ea4717cda6583ef55cf
gwynncausing <gwynn.causing23@gmail.com>
2020-08-28 12:01:35 +0800

    Correct sign in on landing page, Add tabs for dashboard, Add icons to VUD

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -17,7 +17,12 @@ let customerArr = [
     {'dateRegistered': 'August 5, 2016', 'firstName': '5', 'lastName': 'Rete', 'birthDate': 'August 6, 1990', 'city': 'ABC City'}
 ].sort( (firstElement,secondElement) => firstElement['dateRegistered'] > secondElement['dateRegistered']? 1 : -1);
 const buttonElement = `<td class="text-center">
-                        <button type="button" class="btn btn-primary btn-sm mr-2 mb-2 w-auto">View</button>
+                        <button type="button" class="btn btn-primary btn-sm mr-2 mb-2 w-auto">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                            </svg>
+                        </button>
                         <button type="button" class="btn btn-success btn-sm mr-2 mb-2 w-auto">Update</button>
                         <button type="button" class="btn btn-danger btn-sm mr-2  mb-2 w-auto">Delete</button>
                     </td>`;
