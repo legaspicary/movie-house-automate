984879b10e3454d8598e46a2cd1ef31a3b95ce28
Cary <legaspicary@gmail.com>
2020-09-10 21:53:14 +0800

    Separate dashboard templates for dvd and customer, add advance requirements for data tables, fetch data from python instead of js

--- customer/urls.py
+++ customer/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
+from . import views as customer_views
+from dvd import views as dvd_views
 
 app_name = 'customer'
 
 urlpatterns = [
-  path('',views.CustomerView.as_view(), name = "customer_landing_page"),
-  path('registration/', views.registration, name = "customer_registration")
+  path('',customer_views.CustomerView.as_view(), name = "customer_landing_page"),
+  path('dashboard/', customer_views.registration, name = "customer_dashboard"),
+  path('dashboard/customer', customer_views.registration, name = "customer_dashboard"),
+  path('dashboard/dvd', dvd_views.DvdView.as_view(), name = "dvd_dashboard"),
 ]
--- dvd/views.py
+++ dvd/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+dvds = [
+    {'dateRegistered': 'August 12, 2020', 'genre': 'Romance', 'title': 'Kaguya-sama Love is War?', 'releaseDate': 'August 21, 2021', 'price': 1100,'numOfItems':12},
+    {'dateRegistered': 'August 13, 2020', 'genre': 'Comedy', 'title': 'Gintama', 'releaseDate': 'August 4, 2021', 'price': 441,'numOfItems':22},
+    {'dateRegistered': 'August 16, 2020', 'genre': 'Action', 'title': 'Shingeki no Kyojin', 'releaseDate': 'August 14, 2021', 'price': 771,'numOfItems':17},
+    {'dateRegistered': 'August 6, 2020', 'genre': 'Fantasy', 'title': 'Re: Zero', 'releaseDate': 'August 3, 2021', 'price': 512,'numOfItems':52},
+]
+class DvdView(View):
+	def get(self,request):
+		context = {
+			'dvds': dvds,
+			'title':'DVD Dashboard'
+		}
+		return render(request, 'dvd/dashboard.html',context)
