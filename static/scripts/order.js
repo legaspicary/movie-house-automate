701e1e8284e64eff6972f174169756b7acd4cc39
Cary <legaspicary@gmail.com>
2020-10-10 22:00:14 +0800

    Register customer and order to admin page, implement create order for order app

--- static/scripts/order.js
+++ static/scripts/order.js
@@ -6,7 +6,7 @@
 'use strict'
 
 //******************************        CUSTOMER by Legaspi C        **************************
-let customer_table = null;
+let order_table = null;
 //data variable to be used for view listener
 let data = null;
 
@@ -47,10 +47,10 @@ function loadOrderTable() {
             >`,
         columnDefs: [
             //removes the sort buttons in the buttons column
-            { orderable: false, "targets": 5 },
+            { orderable: false, "targets": 4 },
             //Adds View Delete buttons to every row in the datatable
             {
-                targets: 5,
+                targets: 4,
                 data: null,
                 defaultContent: `<div class="button-container">
                         <button type="button" class="btn btn-sm mr-2 mb-2 w-auto vud vud-view" data-toggle="modal" data-target="#viewCustomer" onclick="viewCustomer(this)">
@@ -70,11 +70,10 @@ function loadOrderTable() {
         },
         //matches the data to appropriate column
         columns: [
-            { "data": 'date_registered' },
-            { "data": 'firstname' },
-            { "data": 'lastname' },
-            { "data": 'birthdate', "defaultContent": "<i>Not Set</i>" },
-            { "data": 'city', "defaultContent": "<i>Not Set</i>" },
+            { "data": 'date_rented' },
+            { "data": 'dvd_title' },
+            { "data": 'customer_name' },
+            { "data": 'no_of_items'},
         ],
         //Adds data-id attribute to each row
         createdRow: function(row, data, dataIndex) {
@@ -83,7 +82,7 @@ function loadOrderTable() {
         //Adds filter function at footer after the datatable has been initialized
         initComplete: function() {
             //get data for view listener
-            data = customer_table.ajax.json().data;
+            data = order_table.ajax.json().data;
             this.api().columns().every(function() {
                 var column = this;
                 var select = $('<select class="form-control"><option value="">Filter None</option></select>')
@@ -103,9 +102,9 @@ function loadOrderTable() {
         },
 
     }
-    let table = $('#customerTable').DataTable(initialTableSettings);
-    //passing it to customer_table, so that it can be used in loadInitializers function
-    customer_table = table;
+    let table = $('#orderTable').DataTable(initialTableSettings);
+    //passing it to order_table, so that it can be used in loadInitializers function
+    order_table = table;
     //sets the minimum default value for the date range
     $("input[type=date][name$=min]").val(new Date(0).toISOString().slice(0, 10));
     //sets the maximum default value for the date range
@@ -135,10 +134,10 @@ function loadOrderTable() {
     }
 }
 function reloadTable(data) {
-    //customer_table is global variable
-    customer_table.clear().draw();
-    customer_table.rows.add(data);
-    customer_table.columns.adjust().draw();
+    //order_table is global variable
+    order_table.clear().draw();
+    order_table.rows.add(data);
+    order_table.columns.adjust().draw();
 }
 // Successfully adding a customer
 let addSuccess = function (response,form) {
@@ -146,21 +145,22 @@ let addSuccess = function (response,form) {
     data = response.data;
     reloadTable(data);
     //resets the input form
-    document.getElementById('addThumbnail').setAttribute("src","/static/img/profile_default.png");
-    form.reset();
+		form.reset();
+		//----------------------------------------------------------------------------------------
     $('#addCustomer').modal('toggle');
 }
 // Add invalid to input to show validation error
-let emailExists = function(response,email_input){
-    if (response.responseJSON.email == 'unavailable') {
-        email_input.classList.add('is-invalid');
+let no_stocks = function(response,stocks_input){
+    if (response.responseJSON.stock_message == 'unavailable') {
+			stocks_input.classList.add('is-invalid');
     }
 }
 //adding customer
-let addCustomerListener = function(csrf_token,form){
+let createOrderListener = function(csrf_token,form){
     return function () {
-        let email = document.getElementById('addEmail');
-        email.classList.remove('is-invalid');
+				//----------------------------------------------------------------------------------------
+        let number_of_items_input = document.getElementById('addEmail');
+        number_of_items_input.classList.remove('is-invalid');
         if(form.checkValidity()){
             let formData = new FormData(form);//.append('action','add');
             formData.append('operation','create');
@@ -180,167 +180,153 @@ let addCustomerListener = function(csrf_token,form){
                     addSuccess(response,form)
                 },
                 error: function(response){
-                    emailExists(response,email);
+									//-------------------------------------------------------
+									no_stocks(response,number_of_items_input);
                 }
             });
         }
     }
 }
 //Update success
-let updateSucess = function (response,form) {
-    //update data, a global variable, to get most recent entries
-    data = response.data;
-    reloadTable(data);
-    //resets the input form
+// let updateSucess = function (response,form) {
+//     //update data, a global variable, to get most recent entries
+//     data = response.data;
+//     reloadTable(data);
+//     //resets the input form
     
-    //should be replaced with button to remove picture -->> document.getElementById('addThumbnail').setAttribute("src","/static/img/profile_default.png");
-    form.reset();
-    $('#viewCustomer').modal('toggle');
-}
+//     //should be replaced with button to remove picture -->> document.getElementById('addThumbnail').setAttribute("src","/static/img/profile_default.png");
+// 		form.reset();
+// 		//------------------------------------------------------------------------------------------------------------
+//     $('#viewCustomer').modal('toggle');
+// }
 //updating customer
-let updateCustomerListener = function (csrf_token,form) {
-    return function () {
-        let email = document.getElementById('email');
-        email.classList.remove('is-invalid');
-        if(form.checkValidity()){
-            let formData = new FormData(form);//.append('id',viewedCustomerID);//.append('action','add');
-            formData.append('operation','update');
-            formData.append('id',viewedCustomerID);
-            formData.append('csrfmiddlewaretoken',csrf_token);
-            $.ajax({
-                url: '',
-                type: 'post',
-                //data to be passed to django view
-                data: formData,
-                contentType: false,
-                processData: false,
-                //when successful, change the data in table with new data from server
-                success: function(response){
-                    //console.log('SUBMIT OK');
-                    updateSucess(response,form);
-                },
-                error: function(response){
-                    //console.log('SUBMIT NOT OK');
-                    emailExists(response,email);
-                }
-            });
-        }
-    }
-}
-//Update success
-let deleteSuccess = function (response,form) {
-    //update data, a global variable, to get most recent entries
-    data = response.data;
-    reloadTable(data);
-    //resets the input form
+//------------------------------------------------------------------------
+// let updateCustomerListener = function (csrf_token,form) {
+//     return function () {
+//         let email = document.getElementById('email');
+//         email.classList.remove('is-invalid');
+//         if(form.checkValidity()){
+//             let formData = new FormData(form);//.append('id',viewedCustomerID);//.append('action','add');
+//             formData.append('operation','update');
+//             formData.append('id',viewedCustomerID);
+//             formData.append('csrfmiddlewaretoken',csrf_token);
+//             $.ajax({
+//                 url: '',
+//                 type: 'post',
+//                 //data to be passed to django view
+//                 data: formData,
+//                 contentType: false,
+//                 processData: false,
+//                 //when successful, change the data in table with new data from server
+//                 success: function(response){
+//                     //console.log('SUBMIT OK');
+//                     updateSucess(response,form);
+//                 },
+//                 error: function(response){
+//                     //console.log('SUBMIT NOT OK');
+//                     emailExists(response,email);
+//                 }
+//             });
+//         }
+//     }
+// }
+
+// let deleteSuccess = function (response,form) {
+//     //update data, a global variable, to get most recent entries
+//     data = response.data;
+//     reloadTable(data);
+//     //resets the input form
     
-    //should be replaced with button to remove picture -->> document.getElementById('addThumbnail').setAttribute("src","/static/img/profile_default.png");
-    form.reset();
-    $('#deleteCustomer').modal('toggle');
-}
+//     //should be replaced with button to remove picture -->> document.getElementById('addThumbnail').setAttribute("src","/static/img/profile_default.png");
+// 		form.reset();
+// 		///------------------------------------------------------
+//     $('#deleteCustomer').modal('toggle');
+// }
 //updating customer
-let deleteCustomerListener = function (csrf_token,form) {
-    return function () {
-        let formData = new FormData(form);//.append('id',viewedCustomerID);//.append('action','add');
-        formData.append('operation','delete');
-        formData.append('id',viewedCustomerID);
-        formData.append('csrfmiddlewaretoken',csrf_token);
-        $.ajax({
-            url: '',
-            type: 'post',
-            //data to be passed to django view
-            data: formData,
-            contentType: false,
-            processData: false,
-            //when successful, change the data in table with new data from server
-            success: function(response){
-                deleteSuccess(response,form);
-            },
-            error: function(response){
-                console.log('Delete failed');
-            }
-        });
-    }
-}
+// let deleteCustomerListener = function (csrf_token,form) {
+//     return function () {
+//         let formData = new FormData(form);//.append('id',viewedCustomerID);//.append('action','add');
+//         formData.append('operation','delete');
+//         formData.append('id',viewedCustomerID);
+//         formData.append('csrfmiddlewaretoken',csrf_token);
+//         $.ajax({
+//             url: '',
+//             type: 'post',
+//             //data to be passed to django view
+//             data: formData,
+//             contentType: false,
+//             processData: false,
+//             //when successful, change the data in table with new data from server
+//             success: function(response){
+// 							///------------------------------------------------------
+//                 deleteSuccess(response,form);
+//             },
+//             error: function(response){
+//                 console.log('Delete failed');
+//             }
+//         });
+//     }
+// }
 //Listeners unrelated to table are placed here
 //Variables taken from Django follows python naming convention in this js file (e.g. csrf_token instead of csrfToken)
 function initializeCustomerListeners(csrf_token) {
     //ajax form for add customer
-    let form = document.getElementById('addCustomerForm');
-    form.addEventListener('submit',function(e){e.preventDefault();});
-    $('#addCustomerBtn').click(addCustomerListener(csrf_token,form));
-    let updateForm = document.getElementById('viewCustomerForm');
-    updateForm.addEventListener('submit',function(e){e.preventDefault();});
-    $('#updateCustomerBtn').click(updateCustomerListener(csrf_token,updateForm));
-    let deleteForm = document.getElementById('deleteCustomerForm');
-    deleteForm.addEventListener('submit',function(e){e.preventDefault();});
-    $('#deleteCustomerBtn').click(deleteCustomerListener(csrf_token,updateForm));
+    let addOrderForm = document.getElementById('addOrderForm');
+		addOrderForm.addEventListener('submit',function(e){e.preventDefault();});
+		//------------------------------------------------------------------------------
+    $('#addOrderBtn').click(createOrderListener(csrf_token,form));
+    // let updateForm = document.getElementById('viewCustomerForm');
+    // updateForm.addEventListener('submit',function(e){e.preventDefault();});
+    // $('#updateCustomerBtn').click(updateCustomerListener(csrf_token,updateForm));
+    // let deleteForm = document.getElementById('deleteCustomerForm');
+    // deleteForm.addEventListener('submit',function(e){e.preventDefault();});
+    // $('#deleteCustomerBtn').click(deleteCustomerListener(csrf_token,updateForm));
 }
-let viewedCustomerID = 1;
-//for the view button
-function viewCustomer(button){
-    //get id
-    viewedCustomerID = button.parentNode.parentNode.parentNode.getAttribute("data-id");
-    console.log('ID is '+ viewedCustomerID);
-    let customer = null;
-    for(let i = 0; i < data.length; i++){
-        //data here is the latest json from ajax call
-        if(data[i].id == viewedCustomerID){
-            customer = data[i];
-            //console.log('found you! '+ JSON.stringify(customer));
-            break;
-        }
-    }
-    //get input elements
-    let inputElements = document.getElementById('viewCustomerForm').querySelectorAll("input");
-    //for looping and assigning values rather than manually assigning values to each input
-    let inputName;
-    for (let i = 0; i < inputElements.length; i++) {
-        inputName = inputElements[i].getAttribute("name");
-        //for radio buttons
-        if (inputName == 'status' || inputName == 'gender') {
-            if (inputElements[i].value == customer[inputName]) {
-                //console.log('Checking '+ customer[inputName]);
-                inputElements[i].checked = true;
-            }
-        }
-        //for date types
-        else if (inputName == 'birthdate' && customer[inputName] != null) {
-            inputElements[i].value = new Date(customer[inputName]).toISOString().slice(0, 10);
-        }
-        //for picture
-        else if (inputName == 'profile_picture') {
-            //console.log('profile picture is:'+ customer[inputName])
-            document.getElementById('thumbnail').setAttribute("src",customer[inputName]);
-        } else {
-            //console.log(inputName);
-            inputElements[i].value = customer[inputName];
-        }
-    }
-}
-function deleteCustomer(button){
-    //get id
-    viewedCustomerID = button.parentNode.parentNode.parentNode.getAttribute("data-id");
-    console.log('Delete ID is '+ viewedCustomerID);
-}
-//For view modal
-function uploadImg(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $('#thumbnail').attr('src', e.target.result);
-        }
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-//For create modal
-function addUploadImg(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $('#addThumbnail').attr('src', e.target.result);
-        }
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-// ******************************        CUSTOMER END       **************************
+// let viewedCustomerID = 1;
+// //for the view button
+// function viewCustomer(button){
+//     //get id
+//     viewedCustomerID = button.parentNode.parentNode.parentNode.getAttribute("data-id");
+//     console.log('ID is '+ viewedCustomerID);
+//     let customer = null;
+//     for(let i = 0; i < data.length; i++){
+//         //data here is the latest json from ajax call
+//         if(data[i].id == viewedCustomerID){
+//             customer = data[i];
+//             //console.log('found you! '+ JSON.stringify(customer));
+//             break;
+//         }
+//     }
+//     //get input elements
+//     let inputElements = document.getElementById('viewCustomerForm').querySelectorAll("input");
+//     //for looping and assigning values rather than manually assigning values to each input
+//     let inputName;
+//     for (let i = 0; i < inputElements.length; i++) {
+//         inputName = inputElements[i].getAttribute("name");
+//         //for radio buttons
+//         if (inputName == 'status' || inputName == 'gender') {
+//             if (inputElements[i].value == customer[inputName]) {
+//                 //console.log('Checking '+ customer[inputName]);
+//                 inputElements[i].checked = true;
+//             }
+//         }
+//         //for date types
+//         else if (inputName == 'birthdate' && customer[inputName] != null) {
+//             inputElements[i].value = new Date(customer[inputName]).toISOString().slice(0, 10);
+//         }
+//         //for picture
+//         else if (inputName == 'profile_picture') {
+//             //console.log('profile picture is:'+ customer[inputName])
+//             document.getElementById('thumbnail').setAttribute("src",customer[inputName]);
+//         } else {
+//             //console.log(inputName);
+//             inputElements[i].value = customer[inputName];
+//         }
+//     }
+// }
+// function deleteCustomer(button){
+//     //get id
+//     viewedCustomerID = button.parentNode.parentNode.parentNode.getAttribute("data-id");
+//     console.log('Delete ID is '+ viewedCustomerID);
+// }
+// ******************************        ORDER END       **************************
